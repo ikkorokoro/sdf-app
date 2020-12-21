89a68ef6e7e32fc0e904575e2632cd5758acc635
今吉恒介 <>
2020-12-21 10:57:46 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :account, presence: true
+  validates :email, presence: true
+  validates :passward, presence: true
 end
