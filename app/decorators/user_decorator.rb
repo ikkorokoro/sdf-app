000d938dc94d773dba57a941366cfc9b30698212
今吉恒介 <>
2021-01-13 19:39:03 +0900

    UserDecorator メソッド追加

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module UserDecorator
+  def avatar_image
+    if profile&.avatar&.attached?#画像がアップロードされているかのメソッド
+      profile.avatar
+    else
+      '軍人のアイコン素材.png'
+    end
+  end
+
+  def display_name
+    profile&.name || account
+  end
 end
