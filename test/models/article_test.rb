97dc9d741bdefc534ad01b0de2ae4e7a042b2986
今吉恒介 <>
2021-01-15 17:56:35 +0900

    annotate コミット忘れ２

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  category   :string(255)      not null
+#  content    :string(255)      not null
+#  object     :string(255)      not null
+#  price      :integer          not null
+#  store      :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
+#
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id          :bigint           not null, primary key
+#  affiliation :string(255)
+#  name        :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
 require 'test_helper'
 
 class ProfileTest < ActiveSupport::TestCase
