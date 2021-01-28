be123cccb7cec609168da6dcb321542a5de3d989
今吉恒介 <>
2021-01-28 16:25:04 +0900

    レイアウト変更

--- app/javascript/packs/post_article.js
+++ app/javascript/packs/post_article.js
@@ -1,4 +1,4 @@
-window.addEventListener('turbolinks:load', () => {
+window.addEventListener('load', () => {
   const uploader = document.querySelector('.d-none');
   uploader.addEventListener('change', (e) => {//.noneの値の変更が確定された時
     const file = uploader.files[0];// files[0] 選択したファイルオブジェクトを取得する
