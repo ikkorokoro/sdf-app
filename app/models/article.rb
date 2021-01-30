8dfc10c7086d497941fac34eeda80e4aebba96a5
今吉恒介 <>
2021-01-30 16:32:00 +0900

    rate image カラム　バリデーション追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,7 @@
 #  content    :string(255)      not null
 #  object     :string(255)      not null
 #  price      :integer          not null
+#  rate       :integer
 #  store      :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -25,6 +26,17 @@ class Article < ApplicationRecord
   validates :price, presence: true, length: { maximum: 6 }
   validates :store, presence: true, length: { maximum: 10 }
   validates :category, presence: true
+  validates :rate, presence: true
   validates :content, presence: true, length: { maximum: 100 }
+  validate :image_presence
 
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
 end
