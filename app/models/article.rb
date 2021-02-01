44040216f2d76bfa1b26326cc689a9a49fbf3a0d
今吉恒介 <>
2021-02-01 16:34:54 +0900

    tag_ids デモデータ 追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -32,14 +32,14 @@ class Article < ApplicationRecord
   validates :category_id, presence: true
   validates :rate, presence: true
   validates :content, presence: true, length: { maximum: 100 }
-  validate :image_presence
-  def image_presence
-    if image.attached?
-      if !image.content_type.in?(%('image/jpeg image/png'))
-        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
-      end
-    else
-      errors.add(:image, 'ファイルを添付してください')
-    end
-  end
+  # validate :image_presence
+  # def image_presence
+  #   if image.attached?
+  #     if !image.content_type.in?(%('image/jpeg image/png'))
+  #       errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+  #     end
+  #   else
+  #     errors.add(:image, 'ファイルを添付してください')
+  #   end
+  # end
 end
