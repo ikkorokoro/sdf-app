b7802e5c260a33f11489b9885e2f8bd8143cc442
今吉恒介 <>
2021-02-05 13:19:25 +0900

    font-awe-some 導入2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,10 @@ require("@rails/activestorage").start()
 require("channels")
 require("packs/raty")
 window.$ = window.jQuery = require('jquery');
+import "../stylesheets/application.scss";
 import 'bootstrap-material-design'
-import '../stylesheets/application'
+import '@fortawesome/fontawesome-free/js/all';
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
