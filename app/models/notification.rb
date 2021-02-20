59c99702bd8bea6b39570f971b6931b05ca17cc0
今吉恒介 <>
2021-02-20 15:20:11 +0900

    記述ミス　修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -14,7 +14,7 @@
 #
 class Notification < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
-  belongs_to :post, optional: true
+  belongs_to :article, optional: true
   belongs_to :comment, optional: true
 
   belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
