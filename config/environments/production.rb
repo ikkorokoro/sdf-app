a025b23081dc944e5aa0fc0d5543b8a5572693e6
今吉恒介 <>
2021-03-01 15:46:56 +0900

    本番環境 未設定によるエラー解消

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
-
+  Rails.application.routes.default_url_options[:host] = 'sdf-app'
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
