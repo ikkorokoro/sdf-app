ae0ef723b507f6e1c9a9ee79c7520075a0bff546
今吉恒介 <ikkorokoro@icloud.com>
2021-03-22 22:27:56 +0900

    notification N+1問題解消

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,8 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications
+    @notifications = current_user.passive_notifications.includes(:visiter, :visited, :article)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
   end
-
 end
