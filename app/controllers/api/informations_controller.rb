d5d2f4b6d99416e25f026e6c8655d3c8483b29b8
今吉恒介 <ikkorokoro@icloud.com>
2021-03-27 19:10:11 +0900

    InformationsController 変数名変更

--- app/controllers/api/informations_controller.rb
+++ app/controllers/api/informations_controller.rb
@@ -2,9 +2,9 @@ class Api::InformationsController < Api::ApplicationController
 
   def index
     user = User.find(params[:account_id])
-    has_status = current_user.has_followed?(user)
-    followers = user.followers.count
-    followings = user.followings.count
-    render json: {hasFollow: has_status, followers: followers, followings: followings }
+    has_followed = current_user.has_followed?(user)
+    followers_count = user.followers.count
+    followings_count = user.followings.count
+    render json: { hasFollow: has_followed, followersCount: followers_count, followingsCount: followings_count }
   end
 end
