cd5eafcd6b75b7a5c189f768bdd2bde7e55bad18
今吉恒介 <ikkorokoro@icloud.com>
2021-03-27 17:41:33 +0900

    commments_controller.rb 書き換え

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,25 +1,25 @@
 class Api::CommentsController < Api::ApplicationController
-  before_action: :set_article, only: [:index, :create]
+  before_action :set_article, only: [:index, :create]
   
   def index
-    comments = article.comments
+    comments = @article.comments
     render json: comments
   end
 
   def create
-    comment = article.comments.build(comment_params)
+    comment = @article.comments.build(comment_params)
     comment.save!
     #通知作成
-    article.create_notification_comment!(current_user, comment.id)
+    @article.create_notification_comment!(current_user, comment.id)
     render json: comment
   end
 
   private
-  def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+  def set_article
+    @article = Article.find(params[:article_id])
   end
 
-  def set_article
-    article = Article.find(params[:article_id])
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 end
