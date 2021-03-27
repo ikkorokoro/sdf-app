9b989655872ee526b5751208a0cc14b2e9ea71fb
今吉恒介 <ikkorokoro@icloud.com>
2021-03-27 21:25:47 +0900

    likescontroller 変数名　変更

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,27 +1,30 @@
 class Api::LikesController < Api::ApplicationController
   before_action :authenticate_user!, only: [:show, :create, :destroy]
+  before_action :set_article, only: [:show, :create, :destroy]
+  
   def show
-    article = Article.find(params[:article_id])
-    like_status = current_user.has_liked?(article)
-    likes_count = article.likes.count
-    render json: { hasLiked: like_status, likesCount: likes_count }
+    has_liked = current_user.has_liked?(@article)
+    likes_count = @article.likes.count
+    render json: { hasLiked: has_liked, likesCount: likes_count }
   end
 
   def create
-    @article = Article.find(params[:article_id])
     @article.likes.create!(user_id: current_user.id)
     #通知作成
     @article.create_notification_like!(current_user)
     likes_count = @article.likes.count
     render json: { status: 'ok', likesCount: likes_count }
-
   end
 
   def destroy
-    article = Article.find(params[:article_id])
-    like = article.likes.find_by!(user_id: current_user.id)
+    like = @article.likes.find_by!(user_id: current_user.id)
     like.destroy
     likes_count = article.likes.count
     render json: { status: 'ok', likesCount: likes_count }
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:article_id])
   end
 end
