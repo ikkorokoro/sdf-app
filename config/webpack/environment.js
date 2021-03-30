a7c4f70a15d20541734363a19d2f379e134bf438
今吉恒介 <ikkorokoro@icloud.com>
2021-03-30 17:16:55 +0900

    node-sass非推奨のためdart-sassに乗り換え

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,17 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack') 
-  environment.plugins.append('Provide', 
-    new webpack.ProvidePlugin({ 
-      $: 'jquery', 
-      jQuery: 'jquery', 
-      Popper: ['popper.js', 'default'] 
-    }) 
-  ) 
+environment.plugins.append('Provide', 
+  new webpack.ProvidePlugin({ 
+    $: 'jquery', 
+    jQuery: 'jquery', 
+    Popper: ['popper.js', 'default'] 
+  }) 
+) 
 
+  const sassLoader = environment.loaders.get('sass');
+  // using dart-sass
+  const sassLoaderConfig = sassLoader.use.find(function(element) {
+    return element.loader === 'sass-loader';
+  });
+  sassLoaderConfig.options.implementation = require('sass');
 module.exports = environment
