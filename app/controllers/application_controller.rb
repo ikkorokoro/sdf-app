502c2a45805912a8f4b08678af0f7ac1a0e56001
今吉恒介 <ikkorokoro@icloud.com>
2021-06-08 16:31:13 +0900

    sert_qメソッド 全コントローラー共通化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_q
   def current_user
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
     super
@@ -9,5 +9,9 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:account])
+  end
+
+  def set_q
+    @q = Article.with_attached_image.includes(:tags, user: [profile: [avatar_attachment: :blob]]).ransack(params[:q])
   end
 end
