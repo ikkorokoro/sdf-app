36613570d48c19d5a12ba20a3c7ee9ad63c0f631
今吉恒介 <ikkorokoro@icloud.com>
2021-06-08 23:34:21 +0900

    headerにカテゴリからの検索 タグ検索するためにメソッド追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :all_categorys_and_tags
   before_action :set_q
   def current_user
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
@@ -9,6 +10,13 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:account])
+  end
+
+  def all_categorys_and_tags
+    if user_signed_in?
+      @categorys = Category.all
+      @tags = Tag.all
+    end
   end
 
   def set_q
