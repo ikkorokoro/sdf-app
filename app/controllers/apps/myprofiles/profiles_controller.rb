a226e91d66d37c2c3d04efc5c31f382e73eaa64f
今吉恒介 <ikkorokoro@icloud.com>
2021-06-11 15:01:45 +0900

    プロフィール. おしらせ 自分の投稿 2/2

--- app/controllers/apps/myprofiles/profiles_controller.rb
+++ app/controllers/apps/myprofiles/profiles_controller.rb
@@ -4,7 +4,12 @@ class Apps::Myprofiles::ProfilesController < Apps::Myprofiles::ApplicationContro
   def show
     @profile = current_user.profile
     @articles = current_user.articles.with_attached_image.includes(:tags)
-  end
+    @notifications = current_user.passive_notifications.includes(:visiter, :visited, :article)
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+    @notifications = @notifications.where.not(visiter_id: current_user.id)
+  end 
 
   def edit
   end
